c1d1048293015a662738ad61ac81f073f393ef1b
MasonBarns <clayworley9@gmail.com>
2025-10-30 17:22:08 +0000

    Update layout and styling to match Illinois RP

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ async function updatePlayerCount() {
   try {
     const res = await fetch('/api/server-info');
     const data = await res.json();
-    document.getElementById('players').textContent = data.currentPlayers;
+    document.getElementById('players').textContent = `${data.currentPlayers} Players`;
   } catch (err) {
     document.getElementById('players').textContent = 'Error';
   }
