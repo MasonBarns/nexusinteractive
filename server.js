e9f7cf1ac5b4509c38ffd2e07d7f5a4c18454c58
MasonBarns <clayworley9@gmail.com>
2025-10-30 15:32:18 +0000

    Fix wildcard route for Express

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/api/server-info', async (req, res) => {
 });
 
 // Fallback to index.html
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
